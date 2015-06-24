c6939f84e1789aef85bdf98b5c3d22799ba13ef1
Joel Martin <github@martintribe.org>
2015-06-24 13:26:34 -0500

    repl.js: remove unneeded goog.require

--- repl.js
+++ repl.js
@@ -1,7 +1,5 @@
 require('./.cljs_bootstrap/goog/bootstrap/nodejs.js')
 require('./.cljs_bootstrap/cljs_bootstrap_deps.js')
 
-goog.require('cljs.core');
 goog.require('cljs_bootstrap.repl');
-
 cljs_bootstrap.repl.read_eval_print_loop();
