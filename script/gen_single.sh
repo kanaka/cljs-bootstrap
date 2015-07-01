b8a5953ab6d9d27a51713c84ccf0a4d81cd2bcce
Joel Martin <github@martintribe.org>
2015-07-01 10:17:32 -0500

    Cleanup gen_single.sh
    - Remove some no longer needed hacks.
        - Use closure node bootstrap file directly rather than hard-coded
          partial code
        - Use closure deps file rather than hard coded list
    - Add comments and output and VERBOSE mode

--- script/gen_single.sh
+++ script/gen_single.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-TARGET=repl-all.js
-TOP_DIR=.cljs_bootstrap/goog/
-dep_file=../deps.js
+VERBOSE=${VERBOSE:-}
+TARGET=${TARGET:-repl-all.js}
+TOP_DIR=${TOP_DIR:-.cljs_bootstrap/goog/}
+DEP_FILE=${DEP_FILE:-../deps.js}
 COMPILER_JAR=${COMPILER_JAR:-compiler.jar}
 BASE_PATCH=${BASE_PATCH:-script/base.patch}
+CORE_EDN=${CORE_EDN:-resources/cljs/core.cljs.cache.aot.edn}
+MACROS_EDN=${MACROS_EDN:-.cljs_bootstrap/cljs/core\$macros.cljc.cache.edn}
 
 # Canonicalize paths
 TARGET=$(readlink -f ${TARGET})
@@ -16,22 +19,19 @@ if [ ! -f "${COMPILER_JAR}" ]; then
     exit 1
 fi
 COMPILER_JAR=$(readlink -f ${COMPILER_JAR})
+CORE_EDN=$(readlink -f ${CORE_EDN})
+MACROS_EDN=$(readlink -f ${MACROS_EDN})
+
+echo "Reading main deps file ${TOP_DIR}/${DEP_FILE}"
+deps="$(cat ${TOP_DIR}/${DEP_FILE} | awk -F'"' '{print $2}' | egrep -v '^base.js$')"
 
 
-#goog_deps="$(cat ${TOP_DIR}/deps.js| awk -F"'" '{print $2}')"
-goog_deps="
-    object/object.js
-    debug/error.js
-    dom/nodetype.js
-    string/string.js
-    asserts/asserts.js
-    array/array.js
-    string/stringbuffer.js
-    "
-#deps="$(cat ${TOP_DIR}/${dep_file} | awk -F'"' '{print $2}' | egrep -v '^base.js$|core\$macros.js')"
-deps="$(cat ${TOP_DIR}/${dep_file} | awk -F'"' '{print $2}' | egrep -v '^base.js$')"
+echo "Adding Google Closure Library deps ${TOP_DIR}/deps.js"
+goog_deps="$(cat ${TOP_DIR}/deps.js| awk -F"'" '{print $2}' | egrep -v '^base.js$')"
+# Make sure debug/error.js is included first
+goog_deps="debug/error.js ${goog_deps}"
 
-# Google Closure deps if they actually exist
+# Only add google closure deps that actually appear in the tree
 real_goog_deps=""
 for gdep in ${goog_deps}; do
     if [ -f "${TOP_DIR}/${gdep}" ]; then
@@ -44,67 +44,57 @@ done
 deps="${real_goog_deps} ${deps}"
 
 
-### Add Closure library node bootstrap and then base.js at beginning
-echo "Patching base.js"
+echo "Adding patched base.js and deps files to the beginning of deps"
 cp ${TOP_DIR}/base.js ${TOP_DIR}/base-node.js
-patch ${TOP_DIR}/base-node.js ${BASE_PATCH}
-#deps="bootstrap/nodejs.js base-node.js deps.js ../deps.js ${deps}"
-#deps="../../node_modules/readline-sync/lib/readline-sync.js base-node.js deps.js ../deps.js ${deps}"
-deps="base-node.js deps.js ../deps.js ${deps}"
+if [ "${VERBOSE}" ]; then
+    patch ${TOP_DIR}/base-node.js ${BASE_PATCH}
+else
+    patch -s ${TOP_DIR}/base-node.js ${BASE_PATCH}
+fi
+deps="base-node.js deps.js ${DEP_FILE} ${deps}"
 
-declare -A dep_map
+echo "Adding Closure node bootstrap to the beginning of deps"
+deps="bootstrap/nodejs.js base-node.js deps.js ../deps.js ${deps}"
 
-echo "Creating ${TARGET}"
+# Start with empty file
 cat /dev/null > ${TARGET}
-
-echo "Adding node bootstrap"
-echo '
-var fs        = require("fs");
-var vm        = require("vm");
-function nodeGlobalRequire(file) {
-  var _module = global.module, _exports = global.exports;
-  global.module = undefined;
-  global.exports = undefined;
-  vm.runInThisContext(fs.readFileSync(file), file);
-  global.exports = _exports;
-  global.module = _module;
-}
-var goog = {nodeGlobalRequire: nodeGlobalRequire};
-global.goog = goog;
-' >> ${TARGET}
 
 cmd="java -jar ${COMPILER_JAR} \
           --compilation_level WHITESPACE_ONLY \
           --formatting PRETTY_PRINT"
-#          --process_common_js_modules \
-#          --common_js_entry_module=.cljs_bootstrap/cljs_bootstrap/repl.js"
+
+# make sure each dep only is included once on the command line
+declare -A dep_map
 for dep in ${deps}; do
     if [ -z "${dep_map[${dep}]}" ]; then
         dep_map[${dep}]=done
-        echo "Adding: ${dep}"
+        [ "${VERBOSE}" ] && echo "Adding: ${dep}"
         cmd="${cmd} --js ${dep}"
     fi
 done
 
-# Grab these before we change directories
-CORE_EDN="$(node -e 'console.log(require("util").inspect(require("fs").readFileSync("resources/cljs/core.cljs.cache.aot.edn", "utf-8")))')"
-MACROS_EDN="$(node -e 'console.log(require("util").inspect(require("fs").readFileSync(".cljs_bootstrap/cljs/core\$macros.cljc.cache.edn", "utf-8")))')"
-
 echo "Doing closure compilation"
 cd ${TOP_DIR}
-echo "${cmd} >> ${TARGET}"
+[ "${VERBOSE}" ] && echo "${cmd} >> ${TARGET}"
 ${cmd} >> ${TARGET} 
 
-echo "Patching up ${TARGET}"
+echo "Enabling NODE_JS setting in ${TARGET}"
 # TODO: Use `--define goog.NODE_JS=true` above if only it would work
 sed -i 's@\(goog.NODE_JS *= *\)false;@\1true;@' ${TARGET}
 
-echo "Adding inline edn caches"
+echo "Loading EDN cache files using node"
+# Grab these before we change directories
+core_edn="$(node -e "console.log(require('util').inspect(require('fs').readFileSync('${CORE_EDN}', 'utf-8')))")"
+macros_edn="$(node -e "console.log(require('util').inspect(require('fs').readFileSync('${MACROS_EDN}', 'utf-8')))")"
+
+echo "Adding inline EDN cache strings"
 echo "
-core_edn_cache = ${CORE_EDN};
-macros_edn_cache = ${MACROS_EDN};
+core_edn_cache = ${core_edn};
+macros_edn_cache = ${macros_edn};
 " >> ${TARGET}
 
 echo "Adding calls to load_cache and read_eval_print_loop"
 echo "cljs_bootstrap.repl.load_edn_caches(core_edn_cache, macros_edn_cache);" >> ${TARGET}
 echo "cljs_bootstrap.repl.read_eval_print_loop();" >> ${TARGET}
+
+echo "Finished: ${TARGET}"
